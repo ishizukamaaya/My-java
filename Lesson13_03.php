c78a02d9c9c3b4ff11bab31971b1b48918bfb910
ishizukamaaya <maayajas@gmail.com>
2021-11-25 21:16:13 +0900

    php

---
+++ Lesson13_03.php
@@ -0,0 +1,13 @@
+<?php
+  class Student {
+    public $name;
+
+    function avg($math, $english) {
+      echo (($math+$english)/2)."\n";
+    }
+  }
+
+  $a001 = new Student();
+  $a001->avg(80,70);
+  $a001->name = "sato";
+  echo $a001->name."\n";
---
+++ Lesson13_05.php
@@ -0,0 +1,20 @@
+<?php
+  class Student {
+    public $name;
+
+    public function __construct($name) {
+      $this->name = $name;
+    }
+
+    function avg($math, $english) {
+      echo (($math+$english)/2)."\n";
+    }
+  }
+
+ 
+  // $a001->avg(80,70);
+  $a001 = new Student("sato");
+  echo $a001->name."\n";
+
+  $a002 = new Student("tanaka");
+  echo $a002->name."\n";
