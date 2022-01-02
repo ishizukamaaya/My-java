1aff1250018ee0a5a0534800ead95dbbd29c0614
ishizukamaaya <maayajas@gmail.com>
2022-01-02 20:09:36 +0900

    java

---
+++ lasson1221.java
@@ -0,0 +1,16 @@
+class Forsample {
+
+  public static void main(String[] args){
+    for(int i=0; i<5; i++){
+      for(int j=0; j<i; j++){
+      System.out.println("");
+    }
+    for(int k=0; k<(5-i)*2-1; k++){
+      System.out.println("*");
+    }
+    System.out.println("\n");
+
+  }
+
+ }
+}
