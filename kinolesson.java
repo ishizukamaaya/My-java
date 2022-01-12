a96b6e6cf5974bd99ccd2ce99d5c7e7a07b92ec5
ishizukamaaya <maayajas@gmail.com>
2022-01-12 22:03:10 +0900

    arr

---
+++ kinolesson.java
@@ -0,0 +1,10 @@
+public class kinolesson {
+  public static void main(final String[] args) {
+    String[][] arr = { { "sato", "suzuki" }, { "takahashi", "tanaka" } };
+
+    System.out.println(arr[0][0]);
+    System.out.println(arr[0][1]);
+    System.out.println(arr[1][0]);
+    System.out.println(arr[1][1]);
+  }
+}
