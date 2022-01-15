f05ab2f3ec12350ff99232705b373fd41df2a862
ishizukamaaya <maayajas@gmail.com>
2022-01-15 12:00:47 +0900

    if

---
+++ kinolesson25.java
@@ -0,0 +1,11 @@
+public class kinolesson25 {
+  public static void main(String[] args) {
+
+    for(int i = 0; i <= 4; i ++) {
+      if (i == 3){
+        continue;
+      }
+      System.out.println(i);
+    }
+  }
+}
---
+++ kinolesson26.java
@@ -0,0 +1,9 @@
+public class kinolesson26 {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 2; i++) {
+      for (int j = 0; j <= 2; j++) {
+        System.out.println(i + "-" + j);
+      }
+    }
+  }
+}
