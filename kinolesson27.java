41db7915d31bda028b77d75bca9a25f571f8c088
ishizukamaaya <maayajas@gmail.com>
2022-01-15 12:12:00 +0900

    sum

---
+++ kinolesson27.java
@@ -0,0 +1,9 @@
+public class kinolesson27 {
+  public static void main(String[] args) {
+    int arr[] = { 2, 4, 6, 8, 10 };
+
+    for (int i = 0; i <= 4; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
